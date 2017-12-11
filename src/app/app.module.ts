f0b2c5d6aa304e940633804b6e84fcce2da0cb71
kmboyle <kevin.michael.boyle@gmail.com>
2017-12-11 18:36:29 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SearchBoxDirective } from './search-box.directive';
+import { SalesDataService } from './sales-data.service';
+import { SalesData2Service } from './sales-data2.service';
+import { InitCapsPipe } from './shared/init-caps.pipe';
+import {LoginModule} from './login/login.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent,
+    SearchBoxDirective,
+    InitCapsPipe
+  ],
+  imports: [
+    BrowserModule,
+    LoginModule
+  ],
+  providers: [SalesDataService, SalesData2Service],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
