07e591cee4f761eca1b4735064b47aa42ce66657
Kevin Boyle <kevin.michael.boyle@gmail.com>
2017-12-31 17:30:55 -0500

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
-import { OrdersComponent } from './orders/orders.component';
-import { SearchBoxDirective } from './search-box.directive';
+
 import { SalesDataService } from './sales-data.service';
 import { SalesData2Service } from './sales-data2.service';
 import { InitCapsPipe } from './shared/init-caps.pipe';
