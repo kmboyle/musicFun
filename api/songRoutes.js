8cf74eed26d68c8253695339a19d44409bd23738
Kevin Boyle <kevin.michael.boyle@gmail.com>
2018-08-02 14:49:53 -0400

    launch attempt

--- api/songRoutes.js
+++ api/songRoutes.js
@@ -4,7 +4,7 @@ const {Readable} = require('stream');
 mongodb = require('mongodb');
 multer = require('multer');
 MongoClient = require('mongodb').MongoClient;
-url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017';
+url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 dbName = 'SongDB';
 songs = require('../api/performances/music.json');
 
