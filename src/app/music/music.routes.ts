23f67e8e1bede271a7583ee4f2e77c10fcfa852b
Kevin Boyle <kevin.michael.boyle@gmail.com>
2018-08-16 15:38:38 -0400

    newSong route

--- src/app/music/music.routes.ts
+++ src/app/music/music.routes.ts
@@ -2,8 +2,11 @@ import { MusicComponent } from './music.component';
 import { Routes, RouterModule } from '@angular/router';
 import { MusicPerformanceComponent } from './music-performance.component';
 import { ModuleWithProviders } from '@angular/core';
+import { FileMgmtComponent } from './file-mgmt.component';
 
 export const routing: ModuleWithProviders  = RouterModule.forChild([
     {path: 'music/:id', component: MusicPerformanceComponent},
+    {path: 'home', component: MusicComponent},
+    {path: 'newSong', component: FileMgmtComponent},
     {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ]);
