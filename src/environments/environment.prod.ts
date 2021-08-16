7ca8512e08a344a0efa84cd54788c8db7ae9c397
Kevin Boyle <kevin.michael.boyle@gmail.com>
2021-08-16 15:22:23 -0400

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  clientId: '154832084236-l6qvqp4od9ijgrhsc75i81netn48vuvo.apps.googleusercontent.com'
+  clientId: '154832084236-l6qvqp4od9ijgrhsc75i81netn48vuvo.apps.googleusercontent.com',
+  baseUrl: ''
 };
